525195fbe201b5195ae4ae93e62137a58751d142
Gmsanjuliano <gmsanjuliano@gmail.com>
2021-07-01 10:58:56 -0300

    Daniels Demands

--- script/index.js
+++ script/index.js
@@ -1,21 +1,11 @@
 window.addEventListener("load", () => {
     const navbar = document.getElementById('navbar');
-    let scrolled = false;
 
     window.onscroll = function () {
-        console.log("onscroll"); 
         if (window.pageYOffset > 100) {
             navbar.classList.remove('top');
-            if (!scrolled) {
-                navbar.style.transform = 'translateY(-70px)';
-            }
-            setTimeout(function () {
-                navbar.style.transform = 'translateY(0px)';
-                scrolled = true;
-            }, 200);
         } else {
             navbar.classList.add('top');
-            scrolled = false;
         }
     }
 });
