6b867d8e8d0d38a5269f99751c9c46b03afb7aa3
Gmsanjuliano <gmsanjuliano@gmail.com>
2021-07-02 10:41:04 -0300

    js

--- script/index.js
+++ script/index.js
@@ -8,5 +8,20 @@ window.addEventListener("load", () => {
             navbar.classList.add('top');
         }
     }
+
+    $('#navbar a, .btn').on('click', function (e) {
+        if (this.hash !== '') {
+            e.preventDefault();
+            
+            const hash = this.hash;
+
+            $('html, body').animate(
+                {
+                    scrollTop: $(hash).offset().top - 100,
+                },
+                800
+            );
+        }
+    })
 });
 
